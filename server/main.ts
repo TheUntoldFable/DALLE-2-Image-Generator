8775297cea5f611f2d5359ee8946c19fcff95a5b
uzun0ff <tsvetomir.uzunov@looming.tech>
2023-03-21 22:38:18 +0200

    Implements backend routes

--- server/main.ts
+++ server/main.ts
@@ -1,9 +1,35 @@
-const express  = require('express')
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./api/connect";
+import  postRoutes from './routers/postRoutes'
+import AIRoutes from './routers/AIRoutes'
 
-const app = express()
+const PORT = "8080";
 
-const PORT = '8080'
+dotenv.config();
 
-app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}`)
-})
+const app = express();
+
+connectDB(process.env.MONGODB_URL)
+
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+app.use('/kurzgezaht/post', postRoutes)
+app.use('/kurzgezaht/aiPost', AIRoutes)
+
+const initServer = async () => {
+    try {
+        app.listen(PORT, () => {
+            console.log(`Example app listening on port ${PORT}`);
+          });
+          
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+initServer()
+
+
+
