a63c30b0dd572af0459351a6edafd9d1b0437d00
uzun0ff <tsvetomir.uzunov@looming.tech>
2023-03-21 22:38:18 +0200

    Implements backend routes

---
+++ server/routers/AIRoutes.ts
@@ -0,0 +1,36 @@
+import express, { ErrorRequestHandler, Request, Response } from "express";
+import * as dotenv from "dotenv";
+import { Configuration, OpenAIApi } from "openai";
+import Post from "../models/post";
+
+dotenv.config();
+
+const AIRoutes = express.Router();
+
+const config = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openAI = new OpenAIApi(config);
+
+AIRoutes.route("/").get(async (req: Request, res: Response) => {
+  const { prompt } = req.body;
+
+  try {
+    const aiRes = await openAI.createImage({
+      prompt,
+      n: 1,
+      size: "1024x1024",
+      response_format: "b64_json",
+    });
+
+    const image = aiRes.data.data[0].b64_json;
+
+    res.status(200).json({ photo: image });
+  } catch (error: any) {
+    console.log(error);
+    res.status(500).send(error?.response.data.message ?? 'Error')
+  }
+});
+
+export default AIRoutes;
